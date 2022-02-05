59c2dfca1837a462b7bd572cece5fc4a3c636f47
Hélvio Peixoto <hfcpeixoto@gmail.com>
2022-02-05 15:46:44 -0300

    Refactor script into a class
    Class SnakeApp has been created

--- snake4.py
+++ snake4.py
@@ -2,160 +2,177 @@ import turtle
 import time
 import random
 
-def godown():
-    if head.direction != "up":
-        head.direction = "down"
-def goup():
-    if head.direction != "down":
-        head.direction = "up"
-def goright():
-    if head.direction != "left":
-        head.direction = "right"
-def goleft():
-    if head.direction != "right":
-        head.direction = "left"
-def automove():
-    if head.direction == "up":
-        y = head.ycor()
-        head.sety(y+20)
-    if head.direction == "down":
-        y = head.ycor()
-        head.sety(y-20)
-    if head.direction == "right":
-        x = head.xcor()
-        head.setx(x+20)
-    if head.direction == "left":
-        x = head.xcor()
-        head.setx(x-20)
-    
+class SnakeApp:
 
-delay = 0.1
-score=0
-high_score=0
+    def __init__(self) -> None:
+        self.delay = 0.1
+        self.score=0
+        self.high_score=0
 
-wn = turtle.Screen()
-wn.title("Snake Game")
-wn.bgcolor("blue")
-wn.setup(width=600, height=600)
-wn.tracer(0)
+        self.init_screen()
 
-head = turtle.Turtle()
-head.speed(0)
-head.shape("square")
-head.color("white")
-head.penup()
-head.goto(0, 0)
-head.direction = "stop"
+        self.init_head()        
+       
+        self.init_food()
 
-food = turtle.Turtle()
-colors = random.choice(["red","lime","magenta"])
-shapes = random.choice(["square","triangle","circle"])
-food.speed(0)
-food.shape(shapes)
-food.color(colors)
-food.penup()
-food.goto(0,100)
+        self.segments = []
 
-segments = []
-
-pen=turtle.Turtle()
-pen.speed(0)
-pen.shape("square")
-pen.color("white")
-pen.penup()
-pen.hideturtle()
-pen.goto(0,250)
-pen.write("Score: {} High score: {}".format(score,high_score),align="center",font=("candara",24,"bold"))
-
+        self.init_pen()
         
-wn.listen()
-wn.onkeypress(goup, "w")
-wn.onkeypress(godown, "s")
-wn.onkeypress(goleft, "a")
-wn.onkeypress(goright, "d")
+        self.timer=1
 
-timer=1
+    def init_screen(self):
+        self.wn = turtle.Screen()
+        self.wn.title("Snake Game")
+        self.wn.bgcolor("blue")
+        self.wn.setup(width=600, height=600)
+        self.wn.tracer(0)
 
+        self.wn.listen()
+        self.wn.onkeypress(self.goup, "w")
+        self.wn.onkeypress(self.godown, "s")
+        self.wn.onkeypress(self.goleft, "a")
+        self.wn.onkeypress(self.goright, "d")
 
-while True:
+    def init_head(self):
+        self.head = turtle.Turtle()
+        self.head.speed(0)
+        self.head.shape("square")
+        self.head.color("white")
+        self.head.penup()
+        self.head.goto(0, 0)
+        self.head.direction = "stop"
 
-    wn.update()
-    time.sleep(delay)
-    
-    if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
-        pen.goto(0,0)
-        pen.write("Game Over", align="center",font=("candara",40,"bold"))
-        time.sleep(2)
-        pen.goto(0,250)      
-        head.goto(0,0)
-        head.direction = "stop"
+    def init_food(self):
+        self.food = turtle.Turtle()
+        self.colors = random.choice(["red","lime","magenta"])
+        self.shapes = random.choice(["square","triangle","circle"])
+        self.food.speed(0)
+        self.food.shape(self.shapes)
+        self.food.color(self.colors)
+        self.food.penup()
+        self.food.goto(0,100)
 
-        for segment in segments:
-            segment.goto(1000,1000)
-        segments.clear()
-        score = 0
-        delay = 0.1
-        pen.clear()
-        pen.write("Score: {} High score: {}".format(score,high_score),align="center",font=("candara",24,"bold"))
-    
-    if head.distance(food) <20:
-        x = random.randint(-270,270)
-        y = random.randint(-270,270)
-        colors = random.choice(["red","lime","magenta"])
-        shapes = random.choice(["square","triangle","circle"])
-        food.shape(shapes)
-        food.color(colors)
-        food.goto(x,y)
+    def init_pen(self):
+        self.pen=turtle.Turtle()
+        self.pen.speed(0)
+        self.pen.shape("square")
+        self.pen.color("white")
+        self.pen.penup()
+        self.pen.hideturtle()
+        self.pen.goto(0,250)
+        self.pen.write("Score: {} High score: {}".format(self.score,self.high_score),align="center",font=("candara",24,"bold"))
+
+    def godown(self):
+        if self.head.direction != "up":
+            self.head.direction = "down"
+    def goup(self):
+        if self.head.direction != "down":
+            self.head.direction = "up"
+    def goright(self):
+        if self.head.direction != "left":
+            self.head.direction = "right"
+    def goleft(self):
+        if self.head.direction != "right":
+            self.head.direction = "left"
+    def automove(self):
+        if self.head.direction == "up":
+            y = self.head.ycor()
+            self.head.sety(y+20)
+        if self.head.direction == "down":
+            y = self.head.ycor()
+            self.head.sety(y-20)
+        if self.head.direction == "right":
+            x = self.head.xcor()
+            self.head.setx(x+20)
+        if self.head.direction == "left":
+            x = self.head.xcor()
+            self.head.setx(x-20)
         
-        new_segment = turtle.Turtle()
-        new_segment.speed(0)
-        new_segment.shape("square")
-        new_segment.color("orange")
-        new_segment.penup()
-        segments.append(new_segment)
-        
-        delay -= 0.001
-        score += 10
-      
-    
-        if score > high_score:
-            high_score = score
-        pen.clear()
-        pen.write("Score: {} High score: {}".format(score,high_score),align="center",font=("candara",24,"bold"))
+    def run(self):
+        while True:
 
-    for index in range(len(segments)-1,0,-1):
-        x = segments[index-1].xcor()
-        y = segments[index-1].ycor()
-        segments[index].goto(x,y)
-    
-    if len(segments)>0:
-        x = head.xcor()
-        y = head.ycor()
-        segments[0].goto(x,y)
-        
-    automove()
-    
-    for segment in segments:
-        if segment.distance(head)<20:
-            pen.goto(0,0)
-            pen.write("Game Over", align="center",font=("candara",40,"bold"))            
-            time.sleep(2)
-            pen.goto(0,250)
-            head.goto(0,0)
-            head.direction = "stop"
+            self.wn.update()
+            time.sleep(self.delay)
             
-            for segment in segments:
-                segment.goto(1000,1000)
-            segments.clear()
-            score = 0
-            delay = 0.1
-        
+            if self.head.xcor()>290 or self.head.xcor()<-290 or self.head.ycor()>290 or self.head.ycor()<-290:
+                self.pen.goto(0,0)
+                self.pen.write("Game Over", align="center",font=("candara",40,"bold"))
+                time.sleep(2)
+                self.pen.goto(0,250)      
+                self.head.goto(0,0)
+                self.head.direction = "stop"
 
+                for segment in self.segments:
+                    segment.goto(1000,1000)
+                self.segments.clear()
+                self.score = 0
+                self.delay = 0.1
+                self.pen.clear()
+                self.pen.write("Score: {} High score: {}".format(self.score,self.high_score),align="center",font=("candara",24,"bold"))
             
-            pen.clear()
-            pen.write("Score: {} High score: {}".format(score,high_score),align="center",font=("candara",24,"bold"))
+            if self.head.distance(self.food) <20:
+                x = random.randint(-270,270)
+                y = random.randint(-270,270)
+                self.colors = random.choice(["red","lime","magenta"])
+                self.shapes = random.choice(["square","triangle","circle"])
+                self.food.shape(self.shapes)
+                self.food.color(self.colors)
+                self.food.goto(x,y)
+                
+                new_segment = turtle.Turtle()
+                new_segment.speed(0)
+                new_segment.shape("square")
+                new_segment.color("orange")
+                new_segment.penup()
+                self.segments.append(new_segment)
+                
+                self.delay -= 0.001
+                self.score += 10
+            
+            
+                if self.score > self.high_score:
+                    self.high_score = self.score
+                self.pen.clear()
+                self.pen.write("Score: {} High score: {}".format(self.score,self.high_score),align="center",font=("candara",24,"bold"))
 
+            for index in range(len(self.segments)-1,0,-1):
+                x = self.segments[index-1].xcor()
+                y = self.segments[index-1].ycor()
+                self.segments[index].goto(x,y)
+            
+            if len(self.segments)>0:
+                x = self.head.xcor()
+                y = self.head.ycor()
+                self.segments[0].goto(x,y)
+                
+            self.automove()
+            
+            for segment in self.segments:
+                if segment.distance(self.head)<20:
+                    self.pen.goto(0,0)
+                    self.pen.write("Game Over", align="center",font=("candara",40,"bold"))            
+                    time.sleep(2)
+                    self.pen.goto(0,250)
+                    self.head.goto(0,0)
+                    self.head.direction = "stop"
+                    
+                    for segment in self.segments:
+                        segment.goto(1000,1000)
+                    self.segments.clear()
+                    self.score = 0
+                    self.delay = 0.1
+                
 
+                    
+                    self.pen.clear()
+                    self.pen.write("Score: {} High score: {}".format(self.score,self.high_score),align="center",font=("candara",24,"bold"))
+
+        self.wn.mainloop()
+        turtle.done()
+
+if __name__ == "__main__":
         
-wn.mainloop()
-turtle.done()
+    app = SnakeApp()
+
+    app.run()
